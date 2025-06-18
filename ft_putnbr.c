53f7540d3dcc340b2b54ae6d0868de91c49eafa7
Weijiang Yang <weiyang@f4r6s1.clusters.42paris.fr>
2025-06-18 16:29:53 +0200

    commit

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: weiyang <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/15 14:36:03 by weiyang           #+#    #+#             */
-/*   Updated: 2025/06/17 16:32:16 by weiyang          ###   ########.fr       */
+/*   Updated: 2025/06/18 16:29:42 by weiyang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,25 @@ int print_sign(int is_negative, t_flags flags) {
     return 0;
 }
 
- 
+int print_padding(int padding_zero, int padding_spaces, int is_negative, t_flags flags)
+{
+	int	len;
+
+	len = 0;
+	if (!flags.zero || flags.dot)
+        {
+            len += put_padding(' ', padding_spaces);
+            len += print_sign(is_negative, flags);
+            len += put_padding('0', padding_zero);
+        }
+        else
+        {
+            len += print_sign(is_negative, flags);
+            len += put_padding('0', padding_spaces);
+        }
+	return (len);   
+}
+
 int ft_putnbr_1(char *nbr, t_flags flags, int padding_zero, int padding_spaces)
 {
     int len = 0;
@@ -51,17 +69,7 @@ int ft_putnbr_1(char *nbr, t_flags flags, int padding_zero, int padding_spaces)
 
     if (!flags.minus)
     {
-        if (!flags.zero || flags.dot)
-        {
-            len += put_padding(' ', padding_spaces);
-            len += print_sign(is_negative, flags);
-            len += put_padding('0', padding_zero);
-        }
-        else
-        {
-	    len += print_sign(is_negative, flags);
-            len += put_padding('0', padding_spaces);
-        }
+     	len += print_padding(padding_zero, padding_spaces, is_negative, flags);
         len += ft_putstr(nbr);
     }
     else
@@ -71,42 +79,42 @@ int ft_putnbr_1(char *nbr, t_flags flags, int padding_zero, int padding_spaces)
         len += ft_putstr(nbr);
         len += put_padding(' ', padding_spaces);
     }
-    return len;
+    return (len);
+}
+
+void cal_padding(int n, t_flags flags, int *padding_zero, int *padding_spaces)
+{
+	int	int_len;
+	int	total_len;
+	
+        if (n < 0)
+                int_len = ft_intlen(n) - 1;
+        else
+                int_len = ft_intlen(n);
+        if (flags.precision > int_len)
+               *padding_zero =  flags.precision - int_len;
+        else
+                *padding_zero = 0;
+        if (n < 0 || flags.plus || flags.space)
+                total_len = int_len + *padding_zero + 1;
+        else
+                total_len = int_len + *padding_zero;
+        if (flags.width > total_len)
+                *padding_spaces = flags.width - total_len;
+        else
+                *padding_spaces = 0;
 }
 	
 int	ft_putnbr(int n, t_flags flags)
 {
 	char	*nbr;
 	int	padding_zero;
-	int	total_len;
 	int	padding_spaces;
 	int	len;
-	int	int_len;
-	int	is_negative;
 
 	len = 0;
-	is_negative = 0;
-	if (n < 0)
-		is_negative = 1;
 	nbr = ft_itoa(n);
-	if (n < 0)
-		int_len = ft_intlen(n) - 1;
-	else
-		int_len = ft_intlen(n);
-	if (flags.dot && flags.precision == 0 && n == 0)
-		nbr[0] = '\0';
-	if (flags.precision > int_len)
-		padding_zero =  flags.precision - int_len;
-	else
-		padding_zero = 0;
-	if (is_negative || flags.plus || flags.space)
-		total_len = int_len + padding_zero + 1;
-	else
-		total_len = int_len + padding_zero;
-	if (flags.width > total_len)
-		padding_spaces = flags.width - total_len;
-	else
-		padding_spaces = 0;
+	cal_padding(n, flags, &padding_zero, &padding_spaces);
 	len += ft_putnbr_1(nbr, flags, padding_zero, padding_spaces);
 	free (nbr);
 	return (len); 
