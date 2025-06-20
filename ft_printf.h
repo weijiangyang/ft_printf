c290179769531a4b71590c228c10cc1c79fb813b
Weijiang Yang <weiyang@f4r7s1.clusters.42paris.fr>
2025-06-20 14:02:49 +0200

    commit

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: weiyang <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/15 14:40:23 by weiyang           #+#    #+#             */
-/*   Updated: 2025/06/20 11:58:03 by weiyang          ###   ########.fr       */
+/*   Updated: 2025/06/20 14:02:37 by weiyang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,14 +34,28 @@ typedef struct s_padding
 	int	zero;
 }	t_padding;
 
+int			ft_atoi(char *s);
+int			ft_intlen_hex(unsigned int nb);
+char		*ft_itoa(int n);
+char		*ft_itoa_hex(unsigned int n, int type);
+char		*ft_itoa_hex_ptr(unsigned long n);
+char		*ft_itoa_unsigned(unsigned int n);
 int			ft_printf(const char *format, ...);
-int			ft_putchar(char c);
-int			ft_puthex(unsigned int nb, int type);
-int			ft_puthex_ptr(unsigned long nb);
-int			ft_putnbr(int nb);
-int			ft_putnbr_unsigned(unsigned int nb);
-int			ft_putptr(void *ptr);
-int			ft_putstr(char *s);
+int			ft_putchar(char c, t_flags flags);
+int			ft_putchar_noflag(char c);
+int			ft_puthex(unsigned int n, int type, t_flags flags);
+int			ft_puthex_ptr(unsigned long n, t_flags flags);
+int			ft_putnbr(int n, t_flags flags);
+int			ft_putnbr_unsigned(unsigned int n, t_flags flags);
+int			ft_putnstr_noflag(char *s, int n);
+int			ft_putptr(void *ptr, t_flags flags);
+int			ft_putstr(char *s, t_flags flags);
+int			ft_putstr_noflag(char *s);
+int			ft_strlen(char *s);
+int			print_padding(int pad_zero, int pad_spaces, int is_negative,
+				t_flags flags);
+int			print_sign(int is_negative, t_flags flags);
+int			put_padding(char c, int count);
 const char	*parse_flags(const char *format, t_flags *flags, va_list args);
 
 #endif
