2d41df22349fa051dc7f1b9388ef40d24168c0bd
Weijiang Yang <weiyang@f4r7s1.clusters.42paris.fr>
2025-06-22 15:52:25 +0200

    commit

--- bonus/ft_putptr_bonus.c
+++ bonus/ft_putptr_bonus.c
@@ -6,7 +6,7 @@
 /*   By: weiyang <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/15 14:38:57 by weiyang           #+#    #+#             */
-/*   Updated: 2025/06/20 17:00:50 by weiyang          ###   ########.fr       */
+/*   Updated: 2025/06/22 15:50:54 by weiyang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_putptr_bonus(void *ptr, t_flags flags)
 	int	count;
 
 	if (!ptr)
-		return (ft_putstr("(nil)"));
+		return (ft_putstr_bonus("(nil)", flags));
 	count = 0;
 	count += ft_puthex_ptr_bonus((unsigned long)ptr, flags);
 	return (count);
