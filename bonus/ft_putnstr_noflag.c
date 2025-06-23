8cba0e31525145e93280b8d002b96ef9221fb705
Weijiang Yang <weiyang@f4r7s1.clusters.42paris.fr>
2025-06-23 09:38:41 +0200

    commit

--- bonus/ft_putnstr_noflag.c
+++ bonus/ft_putnstr_noflag.c
@@ -6,7 +6,7 @@
 /*   By: weiyang <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/19 08:45:53 by weiyang           #+#    #+#             */
-/*   Updated: 2025/06/22 13:49:04 by weiyang          ###   ########.fr       */
+/*   Updated: 2025/06/23 09:38:30 by weiyang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,10 @@ int	ft_putnstr_noflag(char *s, int n, t_flags flags)
 	len = 0;
 	if (!s && flags.dot && flags.precision >= 0 && flags.precision < 6)
 		s = "";
-	else if  (!s && flags.dot && flags.precision >= 6)
+	else if (!s && flags.dot && flags.precision >= 6)
 		s = "(null)";
 	else if (!s && !flags.dot)
-		s = "(null)"; 
+		s = "(null)";
 	while (*s && (n < 0 || len < n))
 	{
 		ft_putchar(*s++);
