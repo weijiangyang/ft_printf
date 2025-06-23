50a1d5877d29e8470e7c0cbf5788aac70a3848db
Weijiang Yang <weiyang@f4r7s1.clusters.42paris.fr>
2025-06-23 09:34:30 +0200

    commit

--- bonus/ft_intlen_unsigned.c
+++ bonus/ft_intlen_unsigned.c
@@ -6,24 +6,23 @@
 /*   By: weiyang <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/20 14:44:11 by weiyang           #+#    #+#             */
-/*   Updated: 2025/06/20 16:54:09 by weiyang          ###   ########.fr       */
+/*   Updated: 2025/06/23 09:34:19 by weiyang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_printf.h"
 
-int     ft_intlen_unsigned(unsigned int nb) 
+int	ft_intlen_unsigned(unsigned int nb)
 {
-        int     len;
+	int	len;
 
-        len = 0;
+	len = 0;
 	if (nb == 0)
 		return (1);
-        while (nb)
-        {
-                nb /= 10; 
-                len ++; 
-        }
-        return (len);
+	while (nb)
+	{
+		nb /= 10;
+		len ++;
+	}
+	return (len);
 }
-
